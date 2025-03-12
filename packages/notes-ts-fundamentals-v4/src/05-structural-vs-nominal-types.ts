a512db84136c1efcbff32c6d1d1c6f4863ac1dba
Deepak <deepakworkpc@gmail.com>
2025-03-13 00:04:24 +0530

    chore: testing static typing?

--- packages/notes-ts-fundamentals-v4/src/05-structural-vs-nominal-types.ts
+++ packages/notes-ts-fundamentals-v4/src/05-structural-vs-nominal-types.ts
@@ -1,31 +1,31 @@
 //* Nominal vs Structural
 
 class Car {
-  make: string
-  model: string
-  year: number
-  isElectric: boolean
+    make: string;
+    model: string;
+    year: number;
+    isElectric: boolean;
 }
 
 class Truck {
-  make: string
-  model: string
-  year: number
-  towingCapacity: number
+    make: string;
+    model: string;
+    year: number;
+    towingCapacity: number;
 }
 
 const vehicle = {
-  make: 'Honda',
-  model: 'Accord',
-  year: 2017,
-}
+    make: "Honda",
+    model: "Accord",
+    year: 2017,
+};
 
 function printCar(car: {
-  make: string
-  model: string
-  year: number
+    make: string;
+    model: string;
+    year: number;
 }) {
-  console.log(`${car.make} ${car.model} (${car.year})`)
+    console.log(`${car.make} ${car.model} (${car.year})`);
 }
 /*
 //printCar(new Car()) //✔️ Fine
@@ -33,4 +33,15 @@ function printCar(car: {
 //printCar(vehicle) //✔️ Fine
 /**/
 
-export default {}
+export default {};
+
+function bar(): undefined | string[] {
+    if (Math.random() > 0.5) return;
+    // TYPE CHECKING
+    // -------------
+    // Is `myStrings` type-equivalent to
+    //     what `bar` states it will return?
+    return ["a"];
+}
+
+console.log(bar());
